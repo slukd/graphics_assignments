64d76111aafa4463d69a3d9739839c5e146d0552
slukd <danielbzd1@gmail.com>
2024-03-17 21:25:26 +0200

    first commit

---
+++ Game/main.cpp
@@ -0,0 +1,33 @@
+#include "InputManager.h"
+// #include "display.h"
+#include "game.h"
+#include "glm\glm.hpp"
+
+int main(int argc,char *argv[])
+{
+	const int DISPLAY_WIDTH = 800;
+	const int DISPLAY_HEIGHT = 800;
+	const float CAMERA_ANGLE = 45.0f;
+	const float NEAR = 1.0f;
+	const float FAR = 100.0f;
+
+	Game *scn = new Game(CAMERA_ANGLE,(float)DISPLAY_WIDTH/DISPLAY_HEIGHT,NEAR,FAR);
+	
+	Display display(DISPLAY_WIDTH, DISPLAY_HEIGHT, "OpenGL");
+	
+	//Init(display);
+	
+	scn->Init();
+
+	display.SetScene(scn);
+
+	while(!display.CloseWindow())
+	{
+		scn->Draw(1,0,scn->BACK,true,false);
+		scn->Motion();
+		display.SwapBuffers();
+		display.PollEvents();		
+	}
+	delete scn;
+	return 0;
+}
